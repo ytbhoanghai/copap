d78d439c2b81742d18c869dc676e3939ecd8b3c6
Hai Nguyen Hoang <hai.nguyen.hoang@tpssoft.com>
2021-10-25 09:42:57 +0700

    integerate firebase messaging - service worker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,18 @@ export const environment = {
   production: false
 };
 
+export const configuration = {
+  firebase: {
+    apiKey: "AIzaSyAbY_wnHWKj1wcvy2uY8gVVcWdXXK6Lcq8",
+    authDomain: "copap-72664.firebaseapp.com",
+    projectId: "copap-72664",
+    storageBucket: "copap-72664.appspot.com",
+    messagingSenderId: "74738016837",
+    appId: "1:74738016837:web:edbea716dd8b5ec994792c",
+    measurementId: "G-FFX0QRS8Z6"
+  }
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
